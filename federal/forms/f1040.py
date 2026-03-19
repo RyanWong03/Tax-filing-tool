dd8807693d0540733c10198e2249630c8b38451c
Ryan <ryanwong935@gmail.com>
2026-03-18 23:56:00 -0400

    Basic refund amount calculation works with my 2025 return.

--- federal/forms/f1040.py
+++ federal/forms/f1040.py
@@ -22,6 +22,72 @@ def calculate(filing_data):
     line_16 = calculate_income_tax(line_15, constants.TAX_BRACKETS[filing_status]) #Tax owed
     print(line_16)
 
+    line_17 = 0 #Placeholder 
+
+    line_18 = line_16 + line_17 
+
+    line_21 = 0 #Placeholder
+
+    line_22 = 0 if line_18 - line_21 < 0 else line_18 - line_21
+
+    line_23 = 0 #Placeholder
+
+    line_24 = line_22 + line_23 #Total tax owed
+    print(f"Total tax owed: ${line_24:.2f}")
+
+    line_25a = 0 #Federal income tax withheld from W-2s
+    line_25b = 0 #Federal income tax withheld from 1099s
+
+    while True:
+        try:
+            line_25a += library.irs_round(float(input("Enter the total federal income tax withheld from your W-2 forms (Box 2 on your W-2): ")))
+
+            more_w2 = None
+            while True:
+                more_w2 = input("Do you have another W-2 to enter? (yes/no): ").strip().lower()
+                if more_w2 == 'no' or more_w2 == 'yes':
+                    break
+                else:
+                    print("Invalid input. Please enter 'yes' or 'no'.")
+            
+            if more_w2 == 'no':
+                break
+        except ValueError:
+            print("Invalid input. Please enter a numeric value for federal income tax withheld from W-2s.")
+
+    while True:
+        try:
+            line_25b += library.irs_round(float(input("Enter the total federal income tax withheld from your 1099 forms (Box 4 on your 1099): ")))
+            
+            more_1099 = None
+            while True:
+                more_1099 = input("Do you have another 1099 to enter? (yes/no): ").strip().lower()
+                if more_1099 == 'no' or more_1099 == 'yes':
+                    break
+                else:
+                    print("Invalid input. Please enter 'yes' or 'no'.")
+            
+            if more_1099 == 'no':
+                break
+        except ValueError:
+            print("Invalid input. Please enter a numeric value for federal income tax withheld from 1099s.")
+
+    line_25d = line_25a + line_25b
+
+    line_26 = 0 #Placeholder
+
+    line_27a = 0 #Placeholder
+    line_28 = 0 #Placeholder
+    line_29 = 0 #Placeholder
+    line_30 = 0 #Placeholder
+    line_31 = 0 #Placeholder
+    line_32 = line_27a + line_28 + line_29 + line_30 + line_31 #Total other payments and refundable credits
+
+    line_33 = line_25d + line_26 + line_32 #Total payments
+
+    line_34 = (line_33 - line_24) if (line_33 > line_24) else 0 #Overpaid amount in taxes
+
+    print(f"Refund amount: ${line_34:.2f}")
 
 def calculate_income_tax(taxable_income, tax_brackets):
     tax = 0
