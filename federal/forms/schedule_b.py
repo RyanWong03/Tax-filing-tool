8dd80005392fe9f32b8b4db3e9ea5813458a6300
Ryan Wong <33325695+RyanWong03@users.noreply.github.com>
2026-05-24 17:10:34 -0400

    Restructure federal schedule b (#12)
    * Created schedule b context class that acts as a data structure holding schedule b data
    
    * Created class for schedule b to store necessary data, slimmed down schedule b functions, renamed them. Haven't tested yet, just pushing
    
    * Added an aggregate schedule b function that calculates all the schedule b class values
    
    * Moved tax context to another file to resolve import errors. Commented out code that fills out f1040 for now
    
    * changed schedule b fillout to fill if either dividends or interest exceed 1500
    
    * Added check for bond interest for schedule b fillout

--- federal/forms/schedule_b.py
+++ federal/forms/schedule_b.py
@@ -1,4 +1,26 @@
-import library
+import library, tax_classes
+
+class schedule_b_context:
+    def __init__(self):
+        #These two will be list of dicts. Each dict will have the keys "payer" and "amounts".
+        self.interest_entries = []
+        self.dividend_entries = []
+
+        #Interest (mainly from 1099-INT forms, but not limited to 1099-INT forms)
+        self.taxable_interest = 0
+        self.early_withdrawal_penalty = 0
+        self.interest_on_savings_bonds = 0
+        self.federal_tax_withheld_interest = 0
+        self.tax_exempt_interest = 0
+
+        #Dividends (mainly from 1099-DIV forms, but not limited to 1099-DIV forms)
+        self.ordinary_dividends = 0
+        self.qualified_dividends = 0
+        self.cap_gain_distributions = 0
+        self.unrecaptured_sec_1250_gain = 0
+        self.federal_tax_withheld_dividends = 0
+        self.section_199a_dividends = 0
+
 
 #Enums for determining which section of Schedule B to fill out
 INTEREST = 0
@@ -17,27 +39,10 @@ DIVIDENDS = 1
 # - Box 13: Bond premium on tax-exempt bond obligations
 # - Box 14: Tax-exempt bond CUSIP no.
 #For boxes 15-17, it's unlikely these will be filled out. If they are, we can handle these manually.
-def total_interest():
-    taxable_interest = 0
-
-    interest_forms = {
-        "payers": [],
-        #Each index is an array. First index stores the interest from anything except U.S. Savings Bonds.
-        #second index stores the interest from U.S. Savings Bonds only. These are exempt from state tax
-        "amounts": [] 
-    }
-
-    form_data = {
-        "taxable_interest": 0,
-        "early_withdrawal_penalty": 0,
-        "interest_on_savings_bonds": 0,
-        "federal_tax_withheld": 0,
-        "tax_exempt_interest": 0
-    }
-
+def collect_1099_int(context: tax_classes.tax_context):
     form_avail = input("Please gather all of your 1099-INT forms. If none, enter 'n', otherwise press Enter. ")
 
-    if form_avail == "n": return form_data
+    if form_avail == "n": return
 
     while True:
         try:
@@ -53,19 +58,15 @@ def total_interest():
             #Exempt federally, taxed by state or local.
             tax_exempt_interest = library.irs_round(float(input("Enter the total tax-exempt interest from the 1099-INT form (Box 8): ")))
 
-            taxable_interest += amount + bond_interest
-            amounts_arr = [library.irs_round(amount), library.irs_round(bond_interest)]
-            interest_forms["payers"].append(payer)
-            interest_forms["amounts"].append(amounts_arr)
+            context.schedule_b.interest_entries.append({
+                "payer": payer,
+                "amount": amount,
+                "bond_interest": bond_interest,
+                "early_withdrawal_penalty": early_withdrawal_penalty,
+                "fed_tax_withheld": fed_tax_withheld,
+                "tax_exempt_interest": tax_exempt_interest
+            })
 
-            #Populating return dict
-            form_data["taxable_interest"] = library.irs_round(amount + bond_interest)
-            form_data["early_withdrawal_penalty"] = early_withdrawal_penalty
-            form_data["interest_on_savings_bonds"] = bond_interest
-            form_data["federal_tax_withheld"] = fed_tax_withheld
-            form_data["tax_exempt_interest"] = tax_exempt_interest
-
-            more_1099_int = None
             while True:
                 more_1099_int = input("Do you have another 1099-INT to enter? (yes/no): ").strip().lower()
                 if more_1099_int == 'no' or more_1099_int == 'yes':
@@ -77,13 +78,6 @@ def total_interest():
                 break
         except ValueError:
             print("Invalid input. Please enter a numeric value for interest.")
-    
-    print(f"Total taxable interest collected: ${form_data["taxable_interest"]:.2f}")
-
-    if taxable_interest > 1500:
-        res = schedule_b_fillout(interest_forms, INTEREST)
-        print(res)
-    return form_data
 
 #Dividends from 1099-DIV forms
 #Only the fields from the form that are expected from me are implemented.
@@ -101,28 +95,10 @@ def total_interest():
 # - Box 12: Exempt-interest dividends
 # - Box 13: Specified private activity bond dividends
 #For boxes 14-16, it's unlikely these will be filled out. If they are, we can handle these manually.
-def total_dividends():
-    total_ordinary_dividends = 0
-
-    dividend_forms = {
-        "payers": [],
-        #Each index is an array. First index stores the ordinary dividends.
-        #Second index stores the qualified dividends.
-        "amounts": []
-    }
-
-    form_data = {
-        "ordinary_dividends": 0,
-        "qualified_dividends": 0,
-        "cap_gain_distributions": 0,
-        "unrecaptured_sec_1250_gain": 0,
-        "federal_tax_withheld": 0,
-        "section_199a_dividends": 0
-    }
-
+def collect_1099_div(context: tax_classes.tax_context):
     form_avail = input("Please gather all of your 1099-DIV forms. If none, enter 'n', otherwise press Enter. ")
 
-    if form_avail == "n": return form_data
+    if form_avail == "n": return
 
     while True:
         try:
@@ -142,20 +118,16 @@ def total_dividends():
             #box 5, section 199a dividends, need to fill out form 8995 with info. check taxable income though, if made enough later on, will need to fill 8995-A instead
             section_199a_dividends = library.irs_round(float(input("Enter the total Section 199A dividends from the 1099-DIV form (Box 5): ")))
 
-            dividends_arr = [ordinary_dividends]
-            dividend_forms["payers"].append(payer)
-            dividend_forms["amounts"].append(dividends_arr)
-            total_ordinary_dividends += ordinary_dividends
+            context.schedule_b.dividend_entries.append({
+                "payer": payer,
+                "ordinary_dividends": ordinary_dividends,
+                "qualified_dividends": qualified_dividends,
+                "cap_gain_distributions": cap_gain_distributions,
+                "unrecaptured_sec_1250_gain": unrecaptured_sec_1250_gain,
+                "fed_tax_withheld": fed_tax_withheld,
+                "section_199a_dividends": section_199a_dividends
+            })
 
-            #Populating return dict
-            form_data["ordinary_dividends"] = ordinary_dividends
-            form_data["qualified_dividends"] = qualified_dividends
-            form_data["cap_gain_distributions"] = cap_gain_distributions
-            form_data["unrecaptured_sec_1250_gain"] = unrecaptured_sec_1250_gain
-            form_data["federal_tax_withheld"] = fed_tax_withheld
-            form_data["section_199a_dividends"] = section_199a_dividends
-
-            more_1099_div = None
             while True:
                 more_1099_div = input("Do you have another 1099-DIV to enter? (yes/no): ").strip().lower()
                 if more_1099_div == 'no' or more_1099_div == 'yes':
@@ -168,23 +140,61 @@ def total_dividends():
         except ValueError:
             print("Invalid input. Please enter a numeric value for dividends.")
     
-    print(f"Total ordinary dividends collected: ${total_ordinary_dividends:.2f}")
+def aggregate_schedule_b(context: tax_classes.tax_context):
+    #Interest portion
+    context.schedule_b.taxable_interest = library.irs_round(
+        sum(e["amount"] + e["bond_interest"] for e in context.schedule_b.interest_entries)
+    )
 
-    if total_ordinary_dividends > 1500:
-        res = schedule_b_fillout(dividend_forms, DIVIDENDS)
-        print(res)
+    context.schedule_b.early_withdrawal_penalty = library.irs_round(
+        sum(e["early_withdrawal_penalty"] for e in context.schedule_b.interest_entries)
+    )
 
-    return form_data
+    context.schedule_b.interest_on_savings_bonds = library.irs_round(
+        sum(e["bond_interest"] for e in context.schedule_b.interest_entries)
+    )
 
-def schedule_b_fillout(form_data, section):
-    fields = []
-    if section == INTEREST:
-        fields.append("Part 1: Interest\n")
-        for index, (payer, amount) in enumerate(zip(form_data["payers"], form_data["amounts"])):
-            fields.append(f"Line {index + 1}: {payer} ... ${(amount[0] + amount[1]):.2f}\n")
-    elif section == DIVIDENDS:
-        fields.append("Part 2: Dividends\n")
-        for index, (payer, amount) in enumerate(zip(form_data["payers"], form_data["amounts"])):
-            fields.append(f"Line {index + 1}: {payer} ... ${amount[0]:.2f}\n")
-    
-    return fields
+    context.schedule_b.federal_tax_withheld_interest = library.irs_round(
+        sum(e["fed_tax_withheld"] for e in context.schedule_b.interest_entries)
+    )
+
+    context.schedule_b.tax_exempt_interest = library.irs_round(
+        sum(e["tax_exempt_interest"] for e in context.schedule_b.interest_entries)
+    )
+
+    #Dividends portion
+    context.schedule_b.ordinary_dividends = library.irs_round(
+        sum(e["ordinary_dividends"] for e in context.schedule_b.dividend_entries)
+    )
+
+    context.schedule_b.qualified_dividends = library.irs_round(
+        sum(e["qualified_dividends"] for e in context.schedule_b.dividend_entries)
+    )
+
+    context.schedule_b.cap_gain_distributions = library.irs_round(
+        sum(e["cap_gain_distributions"] for e in context.schedule_b.dividend_entries)
+    )
+
+    context.schedule_b.unrecaptured_sec_1250_gain = library.irs_round(
+        sum(e["unrecaptured_sec_1250_gain"] for e in context.schedule_b.dividend_entries)
+    )
+
+    context.schedule_b.federal_tax_withheld_dividends = library.irs_round(
+        sum(e["fed_tax_withheld"] for e in context.schedule_b.dividend_entries)
+    )
+
+    context.schedule_b.section_199a_dividends = library.irs_round(
+        sum(e["section_199a_dividends"] for e in context.schedule_b.dividend_entries)
+    )
+        
+def schedule_b_fillout(context: tax_classes.tax_context):
+    has_bond_interest = any(e["bond_interest"] > 0 for e in context.schedule_b.interest_entries)
+
+    if context.schedule_b.taxable_interest > 1500 or context.schedule_b.ordinary_dividends > 1500 or has_bond_interest:
+        print("Part 1: Interest\n")
+        for index, entry in enumerate(context.schedule_b.interest_entries):
+            print(f"Line {index + 1}: {entry['payer']} ... ${(entry['amount'] + entry['bond_interest']):.2f}\n")
+
+        print("Part 2: Dividends\n")
+        for index, entry in enumerate(context.schedule_b.dividend_entries):
+            print(f"Line {index + 1}: {entry['payer']} ... ${entry['ordinary_dividends']:.2f}\n")
---
+++ tax_classes.py
@@ -0,0 +1,5 @@
+import federal.forms.schedule_b
+
+class tax_context():
+    def __init__(self):
+        self.schedule_b = federal.forms.schedule_b.schedule_b_context()
