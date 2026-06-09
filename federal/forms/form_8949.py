c8138d7c70de5e941f2a555f0712c5fea1d63442
Ryan <ryanwong935@gmail.com>
2026-06-08 23:15:54 -0400

    Added prompt for adjustments. Probably need to change, was looking at an old form

--- federal/forms/form_8949.py
+++ federal/forms/form_8949.py
@@ -4,7 +4,11 @@ class form_8949_context:
     def __init__(self):
         self.entries = []
 
-def collect_1099_b(context: tax_context.tax_context):
+#Short term stock sales from 1099-B forms
+#Only the fields from the form that are expected from me are implemented.
+#Not implemented:
+# - Box 
+def collect_1099_b_short_term(context: tax_context.tax_context):
     while True:
         input("Enter the code(s) that are most applicable to you, separated by spaces (Example: A B C)" \
         "(A) Short-term transactions reported on Form(s) 1099-B showing basis was reported to the IRS" \
@@ -14,13 +18,24 @@ def collect_1099_b(context: tax_context.tax_context):
         "(H) Short-term transactions reported on Form(s) 1099-DA showing basis was not reported to the IRS" \
         "(I) Short-term digital asset transactions not reported to you on Form 1099-DA or Form 1099-B")
         
+        print("For each sell in your 1099-B, please provide the following information:")
         try:
             description = input("Enter the description of the property (e.g., '100 shares of XYZ Corp'): ")
             date_acquired = input("Enter the date acquired (MM/DD/YYYY): ")
             date_sold = input("Enter the date sold (MM/DD/YYYY): ")
             proceeds = float(input("Enter the proceeds from the sale: "))
             cost_basis = float(input("Enter the cost basis of the property: "))
-            adjustments = float(input("Enter any adjustments (enter 0 if none): "))
+
+            adjustments = {}
+            adjustment_amount = float(input("Enter any adjustments (enter 0 if none): ")) 
+            adjustments['adjustment_amount'] = adjustment_amount
+
+            adjustment_code = None
+
+            if adjustment_amount > 0:
+                adjustment_code = input("Enter the code provided in box 1f")
+
+            adjustments['adjustment_code'] = adjustment_code
 
             context.form_8949.entries.append({
                 "description": description,
