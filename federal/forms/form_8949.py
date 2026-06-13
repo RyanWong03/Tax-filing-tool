3de433b5be22fcd86b5f06daafb3fdac47b040d2
Ryan <ryanwong935@gmail.com>
2026-06-12 20:58:18 -0400

    Added short term fields for each sale

--- federal/forms/form_8949.py
+++ federal/forms/form_8949.py
@@ -25,38 +25,52 @@ def collect_1099_b_short_term(context: tax_context.tax_context):
         
         codes = short_term_codes.split(' ')
         for code in codes:
-            try:
-                print(f"Enter all sales associated with code {code} from you 1099-B(s): ")
-                description = input("Enter the description of the property (e.g., '100 shares of XYZ Corp'): ")
-                date_acquired = input("Enter the date acquired (MM/DD/YYYY): ")
-                date_sold = input("Enter the date sold (MM/DD/YYYY): ")
-                proceeds = float(input("Enter the proceeds from the sale: "))
-                cost_basis = float(input("Enter the cost basis of the property: "))
-                accrued_market_discount = float(input("Enter any accrued market discount (enter 0 if none): "))
-                wash_sale_loss = float(input("Enter any wash sale loss (enter 0 if none): "))
-                federal_tax_withheld = float(input("Enter any federal tax withheld (enter 0 if none): "))
-                state_tax_withheld = float(input("Enter any state tax withheld (enter 0 if none): "))
-                adjustment_code = None #Really the only code we'll have is 'W' for wash sale
+            print(f"Enter all sales associated with code {code} from you 1099-B(s): ")
+            while True:
+                try:
+                    payer = input("Enter the name of the Brokerage: ")
+                    description = input("Enter the description of the property (e.g., '100 shares of XYZ Corp'): ")
+                    date_acquired = input("Enter the date acquired (MM/DD/YYYY): ")
+                    date_sold = input("Enter the date sold (MM/DD/YYYY): ")
+                    proceeds = float(input("Enter the proceeds from the sale: "))
+                    cost_basis = float(input("Enter the cost basis of the property: "))
+                    accrued_market_discount = float(input("Enter any accrued market discount (enter 0 if none): "))
+                    wash_sale_loss = float(input("Enter any wash sale loss (enter 0 if none): "))
+                    federal_tax_withheld = float(input("Enter any federal tax withheld (enter 0 if none): "))
+                    state_tax_withheld = float(input("Enter any state tax withheld (enter 0 if none): "))
+                    adjustment_code = None #Really the only code we'll have is 'W' for wash sale
 
-                if wash_sale_loss > 0:
-                    adjustment_code = 'W'
-                
-                gain = proceeds - cost_basis + wash_sale_loss
+                    if wash_sale_loss > 0:
+                        adjustment_code = 'W'
+                    
+                    gain = proceeds - cost_basis + wash_sale_loss
 
-                context.form_8949.short_term_entries[code.strip().capitalize()].append({
-                    "description": description,
-                    "date_acquired": date_acquired,
-                    "date_sold": date_sold,
-                    "proceeds": proceeds,
-                    "cost_basis": cost_basis,
-                    "adjustments": wash_sale_loss,  #Can hardcode this for now since we're only handling wash sales. Change in future (if nec.).
-                    "adjustment_code": adjustment_code,
-                    "gain": gain,
-                })
+                    context.form_8949.short_term_entries[code.strip().capitalize()].append({
+                        "description": description,
+                        "date_acquired": date_acquired,
+                        "date_sold": date_sold,
+                        "proceeds": proceeds,
+                        "cost_basis": cost_basis,
+                        "adjustments": wash_sale_loss,  #Can hardcode this for now since we're only handling wash sales. Change in future (if nec.).
+                        "adjustment_code": adjustment_code,
+                        "gain": gain,
+                    })
 
-                while True:
-                    more_1099_b = input("Do you have more 1099-B entries to add? (yes/no): ").strip().lower()
-                    if more_1099_b == 'no' or more_1099_b == 'yes':
-                        break
-                    else:
-                        print("Invalid input. Please enter 'yes' or 'no'.")
+                    if accrued_market_discount > 0:
+                        context.schedule_b.interest_entries.append({
+                            "payer": payer,
+                            "amount": accrued_market_discount,
+                            "bond_interest": 0,
+                            "early_withdrawal_penalty": 0,
+                            "fed_tax_withheld": 0,
+                            "tax_exempt_interest": 0
+                        })
+
+                    while True:
+                        more_1099_b = input("Do you have more 1099-B entries to add? (yes/no): ").strip().lower()
+                        if more_1099_b == 'no' or more_1099_b == 'yes':
+                            break
+                        else:
+                            print("Invalid input. Please enter 'yes' or 'no'.")
+                except ValueError:
+                    print("Invalid input. Please enter a numeric value for interest.")
