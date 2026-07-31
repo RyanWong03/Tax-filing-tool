3fef0edfd2ac3cafe6b89cb99aefc6c0e9a2f8bb
Ryan <ryanwong935@gmail.com>
2026-07-30 20:02:23 -0400

    Added schedule d form and class

---
+++ federal/forms/schedule_d.py
@@ -0,0 +1,13 @@
+import library
+
+class schedule_d_context:
+    def __init__(self):
+        #Short term
+        self.code_a_proceeds_total = 0
+        self.code_b_proceeds_total = 0
+        self.code_c_proceeds_total = 0
+
+        #Long term
+        self.code_d_proceeds_total = 0
+        self.code_e_proceeds_total = 0
+        self.code_f_proceeds_total = 0
--- tax_context.py
+++ tax_context.py
@@ -1,7 +1,9 @@
 import federal.forms.schedule_b
 import federal.forms.form_8949
+import federal.forms.schedule_d
 
 class tax_context():
     def __init__(self):
         self.schedule_b = federal.forms.schedule_b.schedule_b_context()
         self.form_8949 = federal.forms.form_8949.form_8949_context()
+        self.schedule_d = federal.forms.schedule_d.schedule_d_context()
