f0c0ff274f14f4ba4a5092de46b4da77ea26720b
Ryan <ryanwong935@gmail.com>
2026-08-10 16:59:37 -0400

    Finished compute capital loss carryover worksheet

--- federal/forms/schedule_d.py
+++ federal/forms/schedule_d.py
@@ -6,11 +6,13 @@ class schedule_d_context:
         self.code_a_proceeds_total = 0
         self.code_b_proceeds_total = 0
         self.code_c_proceeds_total = 0
+        self.short_term_capital_loss_carryover = 0
 
         #Long term
         self.code_d_proceeds_total = 0
         self.code_e_proceeds_total = 0
         self.code_f_proceeds_total = 0
+        self.long_term_capital_loss_carryover = 0
 
 def aggregate_schedule_d(context):
     #Short term
@@ -25,8 +27,6 @@ def aggregate_schedule_d(context):
 
 def compute_capital_loss_carryover_worksheet(context, prior_year_return):
     local_app_data_dir = library.get_data_dir()
-    previous_tax_year = context.tax_year - 1
-    user_prev_year_tax_return_file = f"{local_app_data_dir} + /tax_return_{previous_tax_year}.json"
     worksheet_file = f"{local_app_data_dir} + /capital_loss_carryover_worksheet_{context.tax_year}.json"
 
     with open(worksheet_file, "w") as wf:
@@ -35,9 +35,31 @@ def compute_capital_loss_carryover_worksheet(context, prior_year_return):
         wf.write(f'    "Line 1: {(line_1 := prior_year_return["form_1040_line_15"])},\n')
         wf.write(f'    "Line 2: {(line_2 := abs(prior_year_return["schedule_d_line_21"]))},\n')
         wf.write(f'    "Line 3: {(line_3 := max(line_1 + line_2, 0))},\n')
-        wf.write(f'    "Line 4: {min(line_2, line_3)},\n')
+        wf.write(f'    "Line 4: {(line_4 := min(line_2, line_3))},\n')
 
         if prior_year_return["schedule_d_line_7"] < 0:
-            wf.write(f'    "Line 5: {abs(prior_year_return["schedule_d_line_7"])},\n')
-            wf.write(f'    "Line 6: {max(prior_year_return["schedule_d_line_15"], 0)},\n')
+            wf.write(f'    "Line 5: {(line_5 := abs(prior_year_return["schedule_d_line_7"]))},\n')
+            wf.write(f'    "Line 6: {(line_6 := max(prior_year_return["schedule_d_line_15"], 0))},\n')
+            wf.write(f'    "Line 7: {(line_7 := line_4 + line_6)},\n')
+            wf.write(f'    "Line 8: {(line_8 := max(line_5 - line_7, 0))},\n')
+
+            if line_8 > 0:
+                context.schedule_d.short_term_capital_loss_carryover = line_8
+        else:
+            wf.write(f'    "Line 5: 0,\n')
+
+        if prior_year_return["schedule_d_line_15"] >= 0:
+            wf.write("}\n")
+            return
+
+        #line 9 starts here
+        wf.write(f'    "Line 9: {(line_9 := abs(prior_year_return["schedule_d_line_15"]))},\n')
+        wf.write(f'    "Line 10: {(line_10 := max(prior_year_return["schedule_d_line_7"], 0))},\n')
+        wf.write(f'    "Line 11: {(line_11 := line_4 - line_5)},\n')
+        wf.write(f'    "Line 12: {(line_12 := line_10 + line_11)},\n')
+        wf.write(f'    "Line 13: {(line_13 := max(line_9 - line_12, 0))},\n')
+
+        if line_13 > 0:
+            context.schedule_d.long_term_capital_loss_carryover = line_13
+            
         wf.write("}\n")
