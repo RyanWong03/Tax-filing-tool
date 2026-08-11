6f282f3a7b7bfa4f381cbf0ede85091ffea60c80
Ryan <ryanwong935@gmail.com>
2026-08-10 22:31:47 -0400

    modified compute capital loss carryover worksheet func to not hardcode json strings and use json dump instead

--- federal/forms/schedule_d.py
+++ federal/forms/schedule_d.py
@@ -1,4 +1,4 @@
-import library, json
+import library, json, os
 
 class schedule_d_context:
     def __init__(self):
@@ -26,40 +26,59 @@ def aggregate_schedule_d(context):
     context.schedule_d.code_f_proceeds_total = library.irs_round(sum(entry["proceeds"] for entry in context.form_8949.long_term_entries["F"]))
 
 def compute_capital_loss_carryover_worksheet(context, prior_year_return):
+    worksheet = {"tax_year": context.tax_year}
+
+    line_1 = prior_year_return["form_1040_line_15"]
+    line_2 = abs(prior_year_return["schedule_d_line_21"])
+    line_3 = max(line_1 + line_2, 0)
+    line_4 = min(line_2, line_3)
+
+    worksheet["line_1"] = line_1
+    worksheet["line_2"] = line_2
+    worksheet["line_3"] = line_3
+    worksheet["line_4"] = line_4
+
+    if prior_year_return["schedule_d_line_7"] < 0:
+        line_5 = abs(prior_year_return["schedule_d_line_7"])
+        line_6 = max(prior_year_return["schedule_d_line_15"], 0)
+        line_7 = line_4 + line_6
+        line_8 = max(line_5 - line_7, 0)
+
+        worksheet["line_5"] = line_5
+        worksheet["line_6"] = line_6
+        worksheet["line_7"] = line_7
+        worksheet["line_8"] = line_8
+
+        if line_8 > 0:
+            context.schedule_d.short_term_capital_loss_carryover = line_8
+    else:
+        line_5 = 0
+        worksheet["line_5"] = line_5
+
+    if prior_year_return["schedule_d_line_15"] >= 0:
+        save_capital_loss_carryover_worksheet(context, worksheet)
+        return
+
+    line_9 = abs(prior_year_return["schedule_d_line_15"])
+    line_10 = max(prior_year_return["schedule_d_line_7"], 0)
+    line_11 = max(line_4 - line_5, 0)
+    line_12 = line_10 + line_11
+    line_13 = max(line_9 - line_12, 0)
+
+    worksheet["line_9"] = line_9
+    worksheet["line_10"] = line_10
+    worksheet["line_11"] = line_11
+    worksheet["line_12"] = line_12
+    worksheet["line_13"] = line_13
+
+    if line_13 > 0:
+        context.schedule_d.long_term_capital_loss_carryover = line_13
+
+    save_capital_loss_carryover_worksheet(context, worksheet)
+
+def save_capital_loss_carryover_worksheet(context, worksheet):
     local_app_data_dir = library.get_data_dir()
-    worksheet_file = f"{local_app_data_dir} + /capital_loss_carryover_worksheet_{context.tax_year}.json"
+    worksheet_file = os.path.join(local_app_data_dir, f"capital_loss_carryover_worksheet_{context.tax_year}.json")
 
-    with open(worksheet_file, "w") as wf:
-        wf.write("{\n")
-        wf.write(f'    "Tax Year: {context.tax_year},\n')
-        wf.write(f'    "Line 1: {(line_1 := prior_year_return["form_1040_line_15"])},\n')
-        wf.write(f'    "Line 2: {(line_2 := abs(prior_year_return["schedule_d_line_21"]))},\n')
-        wf.write(f'    "Line 3: {(line_3 := max(line_1 + line_2, 0))},\n')
-        wf.write(f'    "Line 4: {(line_4 := min(line_2, line_3))},\n')
-
-        if prior_year_return["schedule_d_line_7"] < 0:
-            wf.write(f'    "Line 5: {(line_5 := abs(prior_year_return["schedule_d_line_7"]))},\n')
-            wf.write(f'    "Line 6: {(line_6 := max(prior_year_return["schedule_d_line_15"], 0))},\n')
-            wf.write(f'    "Line 7: {(line_7 := line_4 + line_6)},\n')
-            wf.write(f'    "Line 8: {(line_8 := max(line_5 - line_7, 0))},\n')
-
-            if line_8 > 0:
-                context.schedule_d.short_term_capital_loss_carryover = line_8
-        else:
-            wf.write(f'    "Line 5: 0,\n')
-
-        if prior_year_return["schedule_d_line_15"] >= 0:
-            wf.write("}\n")
-            return
-
-        #line 9 starts here
-        wf.write(f'    "Line 9: {(line_9 := abs(prior_year_return["schedule_d_line_15"]))},\n')
-        wf.write(f'    "Line 10: {(line_10 := max(prior_year_return["schedule_d_line_7"], 0))},\n')
-        wf.write(f'    "Line 11: {(line_11 := line_4 - line_5)},\n')
-        wf.write(f'    "Line 12: {(line_12 := line_10 + line_11)},\n')
-        wf.write(f'    "Line 13: {(line_13 := max(line_9 - line_12, 0))},\n')
-
-        if line_13 > 0:
-            context.schedule_d.long_term_capital_loss_carryover = line_13
-            
-        wf.write("}\n")
+    with open(worksheet_file, "w") as f:
+        json.dump(worksheet, f, indent=4)
