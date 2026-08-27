8eb6683691a504c4a93b2cc0206e0a040f470e76
Ryan <ryanwong935@gmail.com>
2026-08-27 10:06:35 -0400

    Fixed up existing test cases. Will add more

--- tests/test_schedule_b.py
+++ tests/test_schedule_b.py
@@ -35,29 +35,32 @@ def test_aggregate_multiple_interest_entries():
 def test_aggregate_single_dividend_entry():
     context = tax_context.tax_context(2025)
     context.schedule_b.dividend_entries.append({
-        "payer": "Capital One",
+        "payer": "Fidelity",
         "ordinary_dividends": 400,
         "qualified_dividends": 250,
         "cap_gain_distributions": 0,
-        "unrecpatured_sec_1250_gain": 0,
+        "unrecaptured_sec_1250_gain": 0,
         "fed_tax_withheld": 0,
         "section_199a_dividends": 0,
     })
 
     schedule_b.aggregate_schedule_b(context)
-    assert context.schedule_b.ordinary_dividends == 4000
+    assert context.schedule_b.ordinary_dividends == 400
+    assert context.schedule_b.qualified_dividends == 250
 
 def test_aggregate_multiple_dividend_entries():
     context = tax_context.tax_context(2025)
-    context.schedule_b.divide_entries.append({
-        "payer": "Capital One", "amount": 1000.0, "bond_interest": 0.0,
-        "early_withdrawal_penalty": 0, "fed_tax_withheld": 0, "tax_exempt_interest": 0
+    context.schedule_b.dividend_entries.append({
+        "payer": "Fidelity", "ordinary_dividends": 1000, "qualified_dividends": 0,
+        "cap_gain_distributions": 0, "unrecaptured_sec_1250_gain": 0, "fed_tax_withheld": 0,
+        "section_199a_dividends": 0
     })
     context.schedule_b.dividend_entries.append({
-        "payer": "JP Morgan Chase", "amount": 500.0, "bond_interest": 0.0,
-        "early_withdrawal_penalty": 0, "fed_tax_withheld": 0, "tax_exempt_interest": 0
+        "payer": "Vanguard", "ordinary_dividends": 3000, "qualified_dividends": 2500,
+        "cap_gain_distributions": 0, "unrecaptured_sec_1250_gain": 0, "fed_tax_withheld": 0,
+        "section_199a_dividends": 0
     })
 
     schedule_b.aggregate_schedule_b(context)
-
-   # assert context.schedule_b.taxable_interest == 1500
+    assert context.schedule_b.ordinary_dividends == 4000
+    assert context.schedule_b.qualified_dividends == 2500
